d400c329a08e54ad9835c3415ecdcfe0c367393d
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-09-08 21:40:14 -0500

    Added code for routing and states, @home, 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,23 @@
-// Me quedé al terminar la sección Configure the Module with Providers
 (function() {
-     function config($stateProvider, $locationProvider) {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+        .html5Mode({
+            //Removes the #! from the url
+            enabled: true,
+            requireBase: false
+        });
+
+        $stateProvider
+            // When navigating to localhost:3000, the ui-view directive in index.html will load the template associated with the landing state
+            .state ('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            // when we navigate to localhost:3000/album, the ui-view directive in the global file will load the album template
+            .state ('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
      }
 
      angular
