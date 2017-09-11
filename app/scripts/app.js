e40c2a2d7c6ac3091b76fb95dac4092a4c635dc3
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-09-10 21:18:59 -0500

    Added album view and player_bar templates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,7 @@
             //Next block is of the Bloc Jams logo which take one to home
             .state ('logo', {
                 url: 'assets/images/bloc_jams_logo.png',
+                templateUrl: '/templates/landing.html'
             })
             // When we navigate to localhost:3000/album, the ui-view directive in the global file will load the album template
             .state ('album', {
