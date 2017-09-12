5bba3908eb9fb532deadf798a7e76c82d0467d99
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-09-11 22:11:02 -0500

    Added album data Angular-style

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -6,11 +6,11 @@ var albumPicasso = {
     albumArtUrl: 'assets/images/album_covers/01.png',
     // Note: No need to specify extension (.mp3)
     songs: [
-        {title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue'},
-        {title: 'Green', duration: 103.96, audioUrl: 'assets/music/green'},
-        {title: 'Red', duration: 268.45, audioUrl: 'assets/music/red'},
-        {title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
-        {title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
+        {num: 1, title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue'},
+        {num: 2, title: 'Green', duration: 103.96, audioUrl: 'assets/music/green'},
+        {num: 3, title: 'Red', duration: 268.45, audioUrl: 'assets/music/red'},
+        {num: 4, title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
+        {num: 5, title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
     ]
 };
 
