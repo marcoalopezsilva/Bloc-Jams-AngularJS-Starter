887521a31fd5d59b52c3affcf7c560a9fd25dcd6
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-09-12 21:27:19 -0500

    Finished coding checkpoint7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,19 +3,23 @@
         var SongPlayer = {};
 
         var currentSong = null;
+
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
 
-        SongPlayer.play = function(song) {
-            if (currentSong !== song) {
-                if (currentBuzzObject) {
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                } else if (currentSong === song) {
-                    if (currentBuzzObject.isPaused()) {
-                        currentBuzzObject.play();
-                    }
-                }
-
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
@@ -23,9 +27,18 @@
 
             currentSong = song;
 
-            currentBuzzObject.play();
-            song.playing = true;
-            }
+        };
+
+        SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+                setSong(song);
+                currentBuzzObject.play();
+                song.playing = true;
+                } else if (currentSong === song) {
+                    if (currentBuzzObject.isPaused()) {
+                        currentBuzzObject.play();
+                    }
+                }
         };
 
         SongPlayer.pause = function(song) {
