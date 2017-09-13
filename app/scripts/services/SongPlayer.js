f092d1560c358005e14db2672cacc963fbfddb6c
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-09-13 15:18:24 -0500

    Added playSong function and documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,15 +2,21 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+        // currentSong is a private attribute because it is available locally (we declare it within this scope)
+        /* @desc Holds information on the current song
+        * @type {Object}
+        */
         var currentSong = null;
 
         /**
+        // currentBuzzObject is declared here, so it is a private (local) attribute
         * @desc Buzz object audio file
         * @type {Object}
         */
         var currentBuzzObject = null;
 
         /**
+        // setSong is a private function (it is declared here)
         * @function setSong
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
@@ -24,16 +30,25 @@
                 formats: ['mp3'],
                 preload: true
             });
-
             currentSong = song;
-
         };
 
+        /**
+        * @function playSong
+        playSong is a private function
+        * @desc Plays the current Buzz object and identifies that the song is, indeed, playing (by setting to true the song.playing attribute)
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+
+        // SongPlayer is a public method: it is not declared here, and thus is available globally
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
                 } else if (currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
                         currentBuzzObject.play();
