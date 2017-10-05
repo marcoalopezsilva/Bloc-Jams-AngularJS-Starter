bbb1b311310560f7e9ddec86001538c7c04b8cc6
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-10-05 15:55:57 -0500

    Added thumbStyle method

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,6 +1,7 @@
 (function () {
     // For directives, the callback function (in this case, seekBar) is a factory function.
     // We've named the directive seekBar, which means Angular will look for seek-bar in the HTML and call this directive when it finds that markup
+    // With Angular, $document must be injected as a dependency if we want to access the  window.document object
     function seekBar($document) {
 
         var calculatePercent = function (seekBar, event) {
@@ -16,6 +17,9 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
+            // A new "isolate" scope is created for the directive's template.
+            // The 'isolate' scope differs from normal scope in that it does not prototypically inherit from its parent scope.
+            // This is useful when creating reusable components, which should not accidentally read or modify data in the parent scope.
             scope: { },
             link: function(scope, element, attributes) {
                 scope.value = 0;
@@ -30,6 +34,7 @@
                      return percent + "%";
                 };
 
+                // When adding properties to the $scope object in the controller, the view (HTML) gets access to these properties
                 scope.fillStyle = function () {
                     return {width: percentString()};
                 };
@@ -42,6 +47,7 @@
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function (event) {
                         var percent = calculatePercent(seekBar, event);
+                        //$scope.$apply() takes a function or an Angular expression string, and executes it, then calls $scope.$digest() to update any bindings or watchers.//
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
                         });
@@ -52,6 +58,10 @@
                     });
                 };
 
+                // This method added for Chekpoint9-Assignment
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
+                };
             }
         };
     }
