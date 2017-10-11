05075122e1e48befa13f1dc4c882f2c479e5b08a
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-10-11 15:18:14 -0500

    Recoded timecode filter to use buzz toTimer

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,24 +1,12 @@
 // Filter functions must return another function which takes at least one argument (the input of the filter – seconds)
 (function() {
-    function timecode() {
+    function timecode(SongPlayer) {
         return function(seconds) {
-            var seconds = Number.parseFloat(seconds);
+            var output = buzz.toTimer(seconds)
 
-            if (Number.isNaN(seconds)) {
-                return '--:--';
-            }
-
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
-
-            var output = minutes + ':';
-
-            if (remainingSeconds <10) {
-                output += '0';
-            }
-
-            output += remainingSeconds;
+            //if (Number.isNaN(seconds)) {
+            //    return '--:--';
+            //}
 
             return output;
 
@@ -27,5 +15,5 @@
 
     angular
         .module('blocJams')
-        .filter('timecode', timecode);
+        .filter('timecode', ['SongPlayer', timecode]);
 })();
